3e6fb5fcdfbea0188f20747193b209da2f26cbc6
Joseph Pereniguez <joseph.pereniguez@epitech.eu>
2020-11-08 22:23:43 +0100

    information style

--- src/containers/CatInformation.view.tsx
+++ src/containers/CatInformation.view.tsx
@@ -14,26 +14,38 @@ class CatInformationView extends Component<IPropsCatInformationView> {
     render(): JSX.Element {
         console.log(this.props.cat)
         return (
-            <div className={"cat-information"}>
-                <div className={"left-part"}>            
-                    <img
-                        className={""}
-                        src={this.props.cat.picturePath}
-                        alt="cat picture"
-                    />
+            <>
+                <button 
+                    className="btn back-button" 
+                    onClick={() => {}}>
+                    Back
+                </button>
+                <div className={"cat-information"}>
+                    <div className={"left-part"}>
+                        <div className={"img-container"}>
+                            <img
+                                src={this.props.cat.picturePath}
+                                alt="cat picture"
+                            />
+                        </div>            
+                    </div>
+                    <div className={"right-part"}>
+                        <div className={"information-container"}>
+                            <h2>{this.props.cat.name}</h2>
+                            <div className={"information"}>
+                                <h3>Birthdate: {this.props.cat.birthdate}</h3>
+                                <h3>Gender: {this.props.cat.gender}</h3>
+                                <h3>Breed: {this.props.cat.breed}</h3>
+                            </div>
+                            <button 
+                                className="btn btn-custom" 
+                                onClick={() => {}}>
+                                Make an appointment to adopt
+                            </button>
+                        </div>
+                    </div>
                 </div>
-                <div className={"right-part"}>
-                    <h2>{this.props.cat.name}</h2>
-                    <h3>Birthdate: {this.props.cat.birthdate}</h3>
-                    <h3>Gender: {this.props.cat.gender}</h3>
-                    <h3>Breed: {this.props.cat.breed}</h3>
-                    <button 
-                        className="btn btn-custom" 
-                        onClick={() => {}}>
-                        Make an appointment to adopt
-                    </button>
-                </div>
-            </div>
+            </>
         );
     }
 }
